12b8530b538e6370a276472e207a1d8d17627c51
zhaoyan.shi <zhaoyan.shi@lseg.com>
2024-05-29 11:00:25 +0800

    update

---
+++ src/leetcode/top100liked/binarytree/MaxDepth.java
@@ -0,0 +1,38 @@
+package leetcode.top100liked.binarytree;
+
+//https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+import java.util.LinkedList;
+import java.util.List;
+
+public class MaxDepth {
+    public int maxDepth_BFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int depth = 0;
+        List<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            List<TreeNode> tmp = new LinkedList<>();
+            for (TreeNode node : nodes) {
+                if(node.left != null) tmp.add(node.left);
+                if(node.right != null) tmp.add(node.right);
+            }
+            nodes = tmp;
+            depth ++;
+        }
+
+
+        return depth;
+    }
+
+    int maxDepth_recursion(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            int leftMaxDepth = maxDepth_recursion(root.left);
+            int rightMaxDepth = maxDepth_recursion(root.right);
+            return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+        }
+    }
+}
---
+++ src/leetcode/top100liked/binarytree/MaxDepth_Test.java
@@ -0,0 +1,30 @@
+package leetcode.top100liked.binarytree;
+
+public class MaxDepth_Test {
+    public static void main(String[] args) {
+        MaxDepth_Test test = new MaxDepth_Test();
+        MaxDepth maxDepth = new MaxDepth();
+
+        int depth;
+        depth = maxDepth.maxDepth( test.buildTree());
+        System.out.println(depth + "[expected 3]");
+
+        depth = maxDepth.maxDepth( null);
+        System.out.println(depth + "[expected 0]");
+
+        depth = maxDepth.maxDepth( new TreeNode(3));
+        System.out.println(depth + "[expected 1]");
+
+    }
+
+    TreeNode buildTree() {
+//        [3,9,20,null,null,15,7] (L1) 3; (L2) 9, 20; (L3) null, null, 15, 7
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20,
+                new TreeNode(15),
+                new TreeNode(7));
+
+        return root;
+    }
+}
